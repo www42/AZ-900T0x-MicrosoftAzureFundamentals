7eb49cefd186dc0ed017ab4b2bbde7c754cbe642
www42 <thomas.jaekel@gfn.de>
2020-01-12 20:34:42 +0100

    create users

---
+++ _tj/CreateAzureADUsers.ps1
@@ -0,0 +1,27 @@
+Get-Module -Name AzureAD -ListAvailable
+Get-Module -Name AzureAD.Standard.Preview -ListAvailable
+
+$Users = Import-Csv -Path ./Users.csv
+
+foreach ($User in $Users) {
+
+    $Displayname = "$User.GivenName $User.Surname"
+    $DomainName = (Get-AzureADDomain).Name
+    $UPN = "$User.GivenName" + '@' + "$DomainName"
+
+    $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $PasswordProfile.Password = $User.Password
+    $PasswordProfile.EnforceChangePasswordPolicy = $true
+    $PasswordProfile.ForceChangePasswordNextLogin = $true
+
+    New-AzureADUser `
+        -AccountEnabled $true `
+        -GivenName $User.GivenName `
+        -Surname $Users.Surname `
+        -DisplayName $Displayname
+        -Department $User.Department `
+        -Country $User.Country `
+        -UsageLocation $User.UsageLocation `
+        -UserPrincipalName $UPN `
+        -PasswordProfile $PasswordProfile
+}
